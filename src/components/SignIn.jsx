a4c829f8894ea737f4d20a93c2f58bb2be994107
Java0807 <uzjava0ff@gmail.com>
2025-02-25 16:47:32 +0500

    first lesson

---
+++ src/components/SignIn.jsx
@@ -0,0 +1,50 @@
+import "../styles/SignIn.css";
+import Image from "next/image";
+
+function SignIn() {
+  return (
+    <div className="all">
+      <form action="">
+        <h1 id="h1">Welcome Back</h1>
+        <div className="inputs">
+          <h3>Email address</h3>
+          <input type="text" placeholder="Enter your email" />
+        </div>
+        <div className="inputs">
+          <h3>Password</h3>
+          <input type="text" placeholder="Name" />
+        </div>
+        <div className="check">
+          <input type="checkbox" />
+          <p>I agree to the terms & policy</p>
+        </div>
+        <button id="singbtn" type="submit">
+          <span>Signup</span>
+        </button>
+        <div className="orhr">
+          <hr />
+          Or <hr />
+        </div>
+        <div className="singWith">
+          <button>
+            <img src="../public/apple.png" alt="" />
+            Sign in with Google
+          </button>
+          <button>
+            <img src="../public/apple.png" alt="" />
+            Sign in with Apple
+          </button>
+        </div>
+        <div className="haveyouacc">
+          <h2 id="h2">
+            Have an account?<span>Sign In</span>
+          </h2>
+        </div>
+      </form>
+      <div className="bgImg">
+        <Image src="/bg.svg" width={1181} height={1042} alt="" />
+      </div>
+    </div>
+  );
+}
+export default SignIn;
---
+++ src/pages/sign-up.js
@@ -0,0 +1,9 @@
+import SignUp from "@/components/SignUp";
+
+export default function RegisterPageUp() {
+  return (
+    <div>
+      <SignUp />
+    </div>
+  );
+}
